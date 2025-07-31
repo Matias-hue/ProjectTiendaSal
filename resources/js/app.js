2d312a275419cd0580a306f5170259975ffba7a5
Matias-hue <ramirezmatias1122@gmail.com>
2025-07-31 20:01:55 -0300

    respurcesjs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,4 @@
 import './bootstrap.js';
-import './inventario.js';
-import './pedidos.js';
-import './registro.js';
-import './resumen.js';
-import './ubicacion.js';
-import './usuarios.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     // Creacion
